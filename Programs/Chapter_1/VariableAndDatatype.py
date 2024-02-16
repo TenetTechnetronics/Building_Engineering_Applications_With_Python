354c5533d8f76d7c94ef1a5d03f49678d179319a
Sathish Kumar Subramaniyan <sathish.s@tavant.com>
2024-02-15 20:32:19 -0500

    program added

---
+++ Programs/Chapter_1/VariableAndDatatype.py
@@ -0,0 +1,20 @@
+x = 10
+print('x is ', type(x))
+
+y = 'Hello'
+print('y is ', type(y))
+
+z = 5.25
+print('z is ', type(z))
+
+isVisible = False
+print('isVisible is ', type(isVisible))
+
+myList = [1, 2, 3, 4]
+print('myList is ', type(myList))
+
+myTuple = (1, 2, 3, 4)
+print('myTuple is ', type(myTuple))
+
+myDict = {'a': 1, 'b': 2} 
+print('myDict is ', type(myDict))
