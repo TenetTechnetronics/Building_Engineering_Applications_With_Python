b6c6ef254e6ceafc27afdaf744cd36f89b8d5bd0
Sathish Kumar Subramaniyan <sathish.s@tavant.com>
2024-02-15 21:25:06 -0500

    conditional statement

---
+++ Programs/Chapter_1/ConditionalStatement.py
@@ -0,0 +1,11 @@
+def compareAge(age):
+    if age < 18:
+        print("Person is  Youth")
+    elif age >10 and age < 60:
+        print("Person is an Adult")
+    else:
+        print("Person is a Senior")
+
+compareAge(10)
+compareAge(32)
+compareAge(65)
