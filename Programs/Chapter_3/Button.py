3104076b6ae57c075efbe0fa0f774afd28af6bdb
Sathish Kumar Subramaniyan <sathish.s@tavant.com>
2024-02-21 23:21:15 -0500

    button label

--- Programs/Chapter_3/Button.py
+++ Programs/Chapter_3/Button.py
@@ -1,4 +1,9 @@
+import os
 from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton
+from PyQt6.QtGui import QIcon
+
+# local machine repository path
+path = os.environ.get('SampleProgram')
 
 class ButtonApp(QWidget):
     def __init__(self):
@@ -7,16 +12,26 @@ class ButtonApp(QWidget):
         self.setGeometry(100, 100, 400, 300)
         
     def initUI(self):
+        layout = QVBoxLayout()
         
+		
         # Create a button
-        self.button = QPushButton('Click Me', self)
+        self.button = QPushButton('Save', self)
         
+		# Create icon with image page
+        icon = QIcon(path + 'assets/images/save_icon.png')
+		# Set icon to button
+        self.button.setIcon(icon)
+        
+        layout.addWidget(self.button)
+        layout.setContentsMargins(100, 100, 100, 100)
         # Create Event handler
         self.button.clicked.connect(self.onButtonClick)
+        self.setLayout(layout)
 
     # Event Handler function
     def onButtonClick(self):
-        print('Button clicked!')
+        print('Save Button clicked!')
 
 if __name__ == '__main__':
     app = QApplication([])
---
+++ Programs/Chapter_3/Label.py
@@ -0,0 +1,43 @@
+import os
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel 
+from PyQt6.QtGui import QFont, QPixmap
+from PyQt6.QtCore import Qt
+
+# local machine repository path
+path = os.environ.get('SampleProgram')
+
+class LabelApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.setGeometry(100, 100, 400, 300)
+        
+    def initUI(self):
+        layout = QVBoxLayout()
+        
+        label = QLabel('Hello, PyQt6!')
+        font = QFont()
+        font.setFamily("Arial")
+        font.setPointSize(25)
+        label.setFont(font)
+        
+        # Set the color, You can use stylesheet to set font also 
+        label.setStyleSheet("color: red")
+        label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        layout.addWidget(label)
+
+        imageLabel = QLabel()
+        # Load the image
+        pixmap = QPixmap(path + 'assets/images/save_icon.png')
+        # Set the image in the QLabel widget
+        imageLabel.setPixmap(pixmap)
+        layout.addWidget(imageLabel)
+
+        layout.setContentsMargins(100, 100, 100, 100)
+        self.setLayout(layout)
+
+if __name__ == '__main__':
+    app = QApplication([])
+    w = LabelApp()
+    w.show()
+    app.exec()
